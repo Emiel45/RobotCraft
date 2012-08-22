cb9a1385d90080bc30915c36b877070d1467aa9d
Emiel Tasseel <Emiel Tasseel>
2012-08-23 01:33:36 +0200

    Mod class template.

---
+++ common/net/brokenpineapple/robotcraft/RobotCraft.java
@@ -0,0 +1,31 @@
+package net.brokenpineapple.robotcraft;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = "RobotCraft")
+@NetworkMod()
+public class RobotCraft {
+
+	@PreInit
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@Init
+	public void init(FMLInitializationEvent event) {
+		
+	}
+	
+	@PostInit
+	public void postInit(FMLPostInitializationEvent event) {
+		
+	}
+	
+}
